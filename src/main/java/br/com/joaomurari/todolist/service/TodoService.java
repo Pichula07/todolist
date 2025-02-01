fe4c15d6f46cfc36ab7088e3049007cb0f6e6a88
joao.murari <joao_pichula@hotmail.com>
2025-02-01 09:15:31 -0300

    /feat: Service package created

---
+++ src/main/java/br/com/joaomurari/todolist/service/TodoService.java
@@ -0,0 +1,43 @@
+package br.com.joaomurari.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import br.com.joaomurari.todolist.domain.Todo;
+import br.com.joaomurari.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> list() {
+        Sort sort = Sort.by(Direction.DESC, "prioridade")
+                .and(Sort.by(Direction.ASC, "id"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
